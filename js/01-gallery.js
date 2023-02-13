94a1acf4594aa3264e8439aaa35c1ffbf4209675
Haidai Kostiantyn <thekos0596@gmail.com>
2023-02-13 14:18:45 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCards(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", onImageClick);
+
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  event.preventDefault();
+  const onCloseModal = (event) => {
+    const ESC_KEY = "Escape";
+
+    if (event.code === ESC_KEY) {
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onCloseModal);
+      },
+
+      onclose: (instance) => {
+        window.removeEventListener("keydown", onCloseModal);
+      },
+    }
+  );
+  instance.show();
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCards(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
